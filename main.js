d7ad92bc369a5348e4fb6578bdbca0277f8807f5
Tommy <pqt0142@gmail.com>
2023-12-29 12:28:19 +0700

    fix bugs

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-//BG
+// Background texture
 import starsTexture from './src/img/stars.jpg';
-//Sun
+// Sun texture
 import sunTexture from './src/img/sun.jpg';
-//Planet
+// Planet texture
 import mercuryTexture from './src/img/mercury.jpg';
 import venusTexture from './src/img/venus.jpg';
 import earthTexture from './src/img/earth.jpg';
@@ -16,34 +16,27 @@ import saturnRingTexture from './src/img/saturn ring.png';
 import uranusTexture from './src/img/uranus.jpg';
 import uranusRingTexture from './src/img/uranus ring.png';
 import neptuneTexture from './src/img/neptune.jpg';
-import plutoTexture from './src/img/pluto.jpg';
 
-//Satellite
+// Satellite
 import earthMoonTexture from './src/img/earthmoon.jpg';
 
-import { AmbientLight, Material } from 'three';
-
-//setting up orbit control
-
-
+// Setting up orbit control
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-//creating a scene to add all elements
-
+// Creating a scene to add all elements
 const scene = new THREE.Scene();
 
-//creating a camera instance
+// Creating a camera instance
 const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
-  0.1,1000
+  0.1, 1000
 );
 
-//setting up texture loader
-
+// Setting up texture loader
 const cubeTextureLoader = new THREE.CubeTextureLoader();
 scene.background = cubeTextureLoader.load([
   starsTexture,
@@ -54,84 +47,76 @@ scene.background = cubeTextureLoader.load([
   starsTexture
 ]);
 
-//setting up orbit control
-
+// Setting up orbit control
 const orbit = new OrbitControls(camera, renderer.domElement);
 camera.position.set(-90, 140, 140);
 orbit.update();
 
-//seting up light
+// Seting up light
 const ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight);
 
-//loading planets
+// Loading planets
 const textureload = new THREE.TextureLoader();
-//sun
+
+// Sun
 const sunGeo = new THREE.SphereGeometry(12, 25, 20);
-const sunMat = new THREE.MeshBasicMaterial({
-  map:textureload.load(sunTexture)
-});
+const sunMat = new THREE.MeshBasicMaterial({ map: textureload.load(sunTexture) });
 const sun = new THREE.Mesh(sunGeo, sunMat);
 scene.add(sun);
 
-//adding point light
-const pointLight = new THREE.PointLight(0xffffff , 3 , 300);
+// Adding point light
+const pointLight = new THREE.PointLight(0xffffff, 3, 300);
 scene.add(pointLight);
 
-//loading another planets now
-//using function
- 
-function createPlanet(size, texture,position, ring, naturalSatellite) {
-    const geometry = new THREE.SphereGeometry(size, 25, 20);
-    const material = new THREE.MeshStandardMaterial({
-      map:textureload.load(texture)
-    });
-    const planet = new THREE.Mesh(geometry, material);
-    const planetObj = new THREE.Object3D;
-    planetObj.add(planet);
-    scene.add(planetObj);
-    planet.position.x = position;
+// Loading another planets now
+function createPlanet(size, texture, position, ring, naturalSatellite) {
+  const geometry = new THREE.SphereGeometry(size, 25, 20);
+  const material = new THREE.MeshStandardMaterial({ map: textureload.load(texture) });
+  const planet = new THREE.Mesh(geometry, material);
+  const planetObj = new THREE.Object3D;
 
-    //Create Planet Ring if need
-    if(ring) {
+  planetObj.add(planet);
+  scene.add(planetObj);
+  planet.position.x = position;
+
+  // Create Planet Ring if need
+  if (ring) {
     const RingGeo = new THREE.RingGeometry(
       ring.innerRadius,
       ring.outerRadius, 30
     );
     const RingMat = new THREE.MeshStandardMaterial({
-      map:textureload.load(ring.texture),
-      side : THREE.DoubleSide
+      map: textureload.load(ring.texture),
+      side: THREE.DoubleSide
     });
 
     const Ring = new THREE.Mesh(RingGeo, RingMat);
     planetObj.add(Ring);
 
     Ring.position.x = position;
-    Ring.rotation.x = -0.5 *Math.PI;
-    }
+    Ring.rotation.x = -0.5 * Math.PI;
+  }
 
-    //Create Natural Satellite Ring if need
-    if(naturalSatellite) {
+  // Create Natural Satellite Ring if need
+  if (naturalSatellite) {
+    const geometry = new THREE.SphereGeometry(naturalSatellite.size, 25, 20);
+    const material = new THREE.MeshStandardMaterial({ map: textureload.load(naturalSatellite.texture) });
 
-      const geometry = new THREE.SphereGeometry(naturalSatellite.size, 25, 20);
-      const material = new THREE.MeshStandardMaterial({
-        map:textureload.load(naturalSatellite.texture)
-      });
+    const satellite = new THREE.Mesh(geometry, material);
+    satellite.position.x = position;
 
-      const satellite = new THREE.Mesh(geometry, material);
-      satellite.position.x = position;
-      
-      const satelliteObj = new THREE.Object3D;
-      satelliteObj.add(satellite)
-      planetObj.add(satelliteObj);
+    const satelliteObj = new THREE.Object3D;
+    satelliteObj.add(satellite)
+    planetObj.add(satelliteObj);
 
-      return {planet, planetObj, satellite, satelliteObj};
+    return { planet, planetObj, satellite, satelliteObj };
+  }
 
-    }
-  return {planet, planetObj};
+  return { planet, planetObj };
 }
 
-function planetMotion(θ,au,e) {
+function planetMotion(θ, au, e) {
   // Tính khoảng cách giữa hành tinh và Mặt Trời
   let r = (au * (1 - e ** 2) / (1 + e * Math.cos(θ)));
 
@@ -141,9 +126,9 @@ function planetMotion(θ,au,e) {
 
   // Trả về vị trí của hành tinh
   return { x, y };
-
 }
 
+// Planet distance from the sun
 const mercuryDistance = 30
 const venusDistance = 50
 const earthDistance = 80
@@ -153,121 +138,124 @@ const saturnDistance = 190
 const uranusDistance = 250
 const neptuneDistance = 300
 
-const mercuryPos = planetMotion(0,mercuryDistance,0.2).x
-const mercury = new createPlanet(4,mercuryTexture,mercuryPos);
+// Ellipse motion setup
+const mercuryPos = planetMotion(0, mercuryDistance, 0.21).x
+const mercury = new createPlanet(4, mercuryTexture, mercuryPos);
 
-const venusPos = planetMotion(0,venusDistance,0.0685).x
-const venus = new createPlanet(5,venusTexture,venusPos);
+const venusPos = planetMotion(0, venusDistance, 0.007).x
+const venus = new createPlanet(5, venusTexture, venusPos);
 
-const earthPos = planetMotion(0,earthDistance,0.016).x
-const earth = new createPlanet(5.56,earthTexture,earthPos,null,{
+const earthPos = planetMotion(0, earthDistance, 0.017).x
+const earth = new createPlanet(5.56, earthTexture, earthPos, null, {
   size: 1.39,
   texture: earthMoonTexture,
   planetDistance: 3,
 });
 
-const marsPos = planetMotion(0,marsDistance,0.09).x
-const mars = new createPlanet(5,marsTexture,marsPos);
+const marsPos = planetMotion(0, marsDistance, 0.093).x
+const mars = new createPlanet(5, marsTexture, marsPos);
 
-const jupiterPos = planetMotion(0,jupiterDistance,0.0489).x
-const jupiter = new createPlanet(6,jupiterTexture,jupiterPos);
+const jupiterPos = planetMotion(0, jupiterDistance, 0.049).x
+const jupiter = new createPlanet(6, jupiterTexture, jupiterPos);
 
-const saturnPos = planetMotion(0,saturnDistance,0.0545).x
-const saturn = new createPlanet(8,saturnTexture,saturnPos,{
+const saturnPos = planetMotion(0, saturnDistance, 0.055).x
+const saturn = new createPlanet(8, saturnTexture, saturnPos, {
   innerRadius: 10,
   outerRadius: 20,
   texture: saturnRingTexture
 });
 
-const uranusPos = planetMotion(0,uranusDistance,0.0466).x
-const uranus = new createPlanet(8.2,uranusTexture,uranusPos,{
+const uranusPos = planetMotion(0, uranusDistance, 0.047).x
+const uranus = new createPlanet(8.2, uranusTexture, uranusPos, {
   innerRadius: 10,
   outerRadius: 20,
   texture: uranusRingTexture
 });
 
-const neptunePos = planetMotion(0,neptuneDistance,0.0085).x
-const neptune = new createPlanet(5,neptuneTexture,neptunePos);
+const neptunePos = planetMotion(0, neptuneDistance, 0.009).x
+const neptune = new createPlanet(5, neptuneTexture, neptunePos);
 
-const planetSpeed = 0.0005 ;
+// Planet speed
+const mercurySpeed = 0.002;
+const venusSpeed = 0.0008;
+const earthSpeed = 0.0005;
+const marsSpeed = 0.00026;
+const jupiterSpeed = 0.00004;
+const saturnSpeed = 0.00002;
+const uranusSpeed = 0.00001;
+const neptuneSpeed = 0.000003;
 
-const earthSpeed = 0.0002;
-const mercurySpeed = earthSpeed*5;
-const venusSpeed = earthSpeed*3;
+function animate() {
+  const mercuryTime = mercurySpeed * performance.now();
+  const venusTime = venusSpeed * performance.now();
+  const earthTime = earthSpeed * performance.now();
+  const marsTime = marsSpeed * performance.now();
+  const jupiterTime = jupiterSpeed * performance.now();
+  const saturnTime = saturnSpeed * performance.now();
+  const uranusTime = uranusSpeed * performance.now();
+  const neptuneTime = neptuneSpeed * performance.now();
 
-function animate(){
+  sun.rotateY(0.02);
 
-  const time = planetSpeed * performance.now(); //time in ms*value to slow orbit
-
-  const earthTime = earthSpeed * performance.now(); 
-  const mercuryTime = mercurySpeed * performance.now(); 
-  const venusTime = venusSpeed * performance.now(); 
-
-  console.log(time)
-
-  sun.rotateY(0.002);
-
-  let mercuryMotion = planetMotion(mercuryTime,mercuryDistance,0.2)
+  let mercuryMotion = planetMotion(mercuryTime, mercuryDistance, 0.2)
   mercury.planet.rotateY(0.001);
   mercury.planetObj.position.x = mercuryMotion.x - mercuryDistance
   mercury.planetObj.position.z = mercuryMotion.y
 
-  let venusMotion = planetMotion(venusTime,venusDistance,0.0685)
+  let venusMotion = planetMotion(venusTime, venusDistance, 0.0685)
   venus.planet.rotateY(0.0012);
   venus.planetObj.position.x = venusMotion.x - venusDistance
   venus.planetObj.position.z = venusMotion.y
 
-  let earthMotion = planetMotion(earthTime,earthDistance,0.016)
+  let earthMotion = planetMotion(earthTime, earthDistance, 0.016)
   earth.planet.rotateY(0.012);
   earth.planetObj.position.x = earthMotion.x - earthDistance
   earth.planetObj.position.z = earthMotion.y
 
   earth.satellite.rotateY(0.012);
   const moonDistance = 9;
-  const moonSpeed = 10; //  disatance between the midlle of the moon and the middle of the earth 
+  const moonSpeed = 10; // Disatance between the midlle of the moon and the middle of the earth 
   const angle = earth.planetObj.rotation.y;
-  earth.satelliteObj.position.x = Math.cos(angle*moonSpeed) * moonDistance;
-  earth.satelliteObj.position.z = Math.sin(angle*moonSpeed) * moonDistance;
+  earth.satelliteObj.position.x = Math.cos(angle * moonSpeed) * moonDistance;
+  earth.satelliteObj.position.z = Math.sin(angle * moonSpeed) * moonDistance;
 
-  let marsMotion = planetMotion(time,marsDistance,0.09)
+  let marsMotion = planetMotion(marsTime, marsDistance, 0.09)
   mars.planet.rotateY(0.013);
   mars.planetObj.position.x = marsMotion.x - marsDistance
   mars.planetObj.position.z = marsMotion.y
 
-  let jupiterMotion = planetMotion(time,jupiterDistance,0.0489)
+  let jupiterMotion = planetMotion(jupiterTime, jupiterDistance, 0.0489)
   jupiter.planet.rotateY(0.04);
   jupiter.planetObj.position.x = jupiterMotion.x - jupiterDistance
   jupiter.planetObj.position.z = jupiterMotion.y
 
-  let saturnMotion = planetMotion(time,saturnDistance,0.0545)
+  let saturnMotion = planetMotion(saturnTime, saturnDistance, 0.0545)
   saturn.planet.rotateY(0.04);
   saturn.planetObj.position.x = saturnMotion.x - saturnDistance
   saturn.planetObj.position.z = saturnMotion.y
 
-  let uranusMotion = planetMotion(time,uranusDistance,0.0466)
+  let uranusMotion = planetMotion(uranusTime, uranusDistance, 0.0466)
   uranus.planet.rotateY(0.012);
   uranus.planetObj.position.x = uranusMotion.x - uranusDistance
   uranus.planetObj.position.z = uranusMotion.y
 
-  let neptuneMotion = planetMotion(time,neptuneDistance,0.0085)
+  let neptuneMotion = planetMotion(neptuneTime, neptuneDistance, 0.0085)
   neptune.planet.rotateY(0.01);
   neptune.planetObj.position.x = neptuneMotion.x - neptuneDistance
   neptune.planetObj.position.z = neptuneMotion.y
 
-
   orbit.update();
 
   renderer.render(scene, camera);
-
 }
+
 renderer.setAnimationLoop(animate);
 
-//setting window size
-
-window.addEventListener('resize', function(){
-  camera.aspect = window.innerWidth/window.innerHeight;
+// Setting window size
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth,window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
 
